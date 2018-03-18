8f2a1b406748953fb51e0c85b8bb3de320c1838e
Katy <ihatetoast@gmail.com>
2018-03-18 00:55:35 -0500

    some svgs and some json

---
+++ README.md
@@ -0,0 +1,6 @@
+# Icky Clicky
+
+Icky Clicky is a memory built on ReactJS.
+
+The user sees a grid made up of images of icky things. The user clicks on an image, and, if the user has not already clicked on the image, the score increases by one point. After every click, the deck is shuffled. The user clicks on another image. As long as it's a new image, the score keeps increasing. Once the user clicks on an image she's already clicked on, the score is reset to 0, and the game begins again.
+
--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import ickies from './ickies.json';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Icky Clicky</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div class="instructions">
+          <p>Click on an icky thing and score points, but don't click on any icky than once!</p>
+        </div>
       </div>
     );
   }
